f1044de46db074429fe64daa30dff498c8dfdf48
Kevin Mera <kamcak@hotmail.es>
2021-03-18 22:54:05 -0500

    model wip

---
+++ src/model/Book.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Book {
+
+	private String ISBNcode;
+	
+	
+	
+}
---
+++ src/model/Shelve.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Shelve {
+	
+	private String ISBNCode;
+	private double price;
+	private int booksQuantity;
+	
+	public Shelve(String iSBNCode, double price, int booksQuantity) {
+		ISBNCode = iSBNCode;
+		this.price = price;
+		this.booksQuantity = booksQuantity;
+	}
+	
+	public Shelve(String iSBNCode, double price) {
+		ISBNCode = iSBNCode;
+		this.price = price;
+		this.booksQuantity = 0;
+	}
+
+	public String getISBNCode() {
+		return ISBNCode;
+	}
+
+	public void setISBNCode(String iSBNCode) {
+		ISBNCode = iSBNCode;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getBooksQuantity() {
+		return booksQuantity;
+	}
+
+	public void setBooksQuantity(int booksQuantity) {
+		this.booksQuantity = booksQuantity;
+	}
+	
+	
+	
+
+}
