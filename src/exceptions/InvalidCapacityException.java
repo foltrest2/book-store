9cbf6e23efd83a1e52fb927e9f7c3eb0242d65e3
Kevin Mera <kamcak@hotmail.es>
2021-03-23 17:28:35 -0500

    Docs folder, diagram and Kevin Mera dashboard added

--- src/exceptions/InvalidCapacityException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-@SuppressWarnings("serial")
-public class InvalidCapacityException extends Exception {
-
-	public InvalidCapacityException() {
-		super("The initial capacity can't be less than 0");
-	}
-}
